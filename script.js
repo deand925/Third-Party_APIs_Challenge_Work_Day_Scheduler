8548be59799e9b30517b9510010d3aae94b9cb2d
deand925 <97762768+deand925@users.noreply.github.com>
2022-08-17 10:01:32 -0700

    created getStored function

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ if(localStorage.getItem('current-date')){
 
 
 localStorage.setItem('current-date', currentDate);
+
+// save text to local storage
+$('.saveBtn').on('click', function(event){
+    let textInput = $(this).siblings('textarea').val();
+    // retrieving the id of the button clicked
+    let inputId = $(this).attr('id')
+    //  saving key and value to local storage
+    localStorage.setItem(inputId, textInput);
+    })
+
+    // start of getStored function to retrieve past events saved to local storage 
+function getStored(){
+    // creating a for loop 
+    for(let i = 0; i < localStorageKey.length; i++){
+        // checking if there is anything saved to local storage
+        if(localStorage.getItem(localStorageKey[i])){
+            // if something is saved to local storage push the key to the keyArr
+            keyArr.push(localStorageKey[i]);
+            // if something is saved to local storage push the value to the valueArr
+            valueArr.push(localStorage.getItem(localStorageKey[i]));
+            console.log((localStorage.getItem(localStorageKey[i])))
+        }
+    }
+
+    // start of getStored function to retrieve past events saved to local storage 
+function getStored(){
+    // creating a for loop 
+    for(let i = 0; i < localStorageKey.length; i++){
+        // checking if there is anything saved to local storage
+        if(localStorage.getItem(localStorageKey[i])){
+            // if something is saved to local storage push the key to the keyArr
+            keyArr.push(localStorageKey[i]);
+            // if something is saved to local storage push the value to the valueArr
+            valueArr.push(localStorage.getItem(localStorageKey[i]));
+            console.log((localStorage.getItem(localStorageKey[i])))
+        }
+    }
